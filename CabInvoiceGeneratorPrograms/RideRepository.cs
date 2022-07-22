75af91495f3777b75990a3252ce28aac9217da96
neeladri22 <neeladri22@gmail.com>
2022-07-22 13:12:15 -0400

    Modified to get list of rides from ride repository and return invoice when given user id -UC4

---
+++ CabInvoiceGeneratorPrograms/RideRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGeneratorPrograms
+{
+    public class RideRepository
+    {
+        Dictionary<int, List<Ride>> userCabRides;
+        Dictionary<int, InvoiceSummary> userCabInvoice;
+
+        public RideRepository()
+        {
+            this.userCabRides = new Dictionary<int, List<Ride>>();
+            this.userCabInvoice = new Dictionary<int, InvoiceSummary>();
+        }
+
+        //UC4 - Method to store invoice of all rides by user id
+        public void AddUserRidesToRepository(int userId, Ride[] rides, RideType rideType)
+        {
+            bool rideList = this.userCabRides.ContainsKey(userId);
+            try
+            {
+                if (!rideList)
+                {
+                    List<Ride> list = new List<Ride>();
+                    list.AddRange(rides);
+                    this.userCabRides.Add(userId, list);
+                    CabInvoiceGenerator generateCabInvoice = new CabInvoiceGenerator(rideType);
+                    InvoiceSummary invoiceSummary = generateCabInvoice.CalculateAgreegateFare(rides);
+                    userCabInvoice.Add(userId, invoiceSummary);
+                }
+            }
+            catch (CabInvoiceGeneratorException)
+            {
+                throw new CabInvoiceGeneratorException(CabInvoiceGeneratorException.ExceptionType.NULL_RIDES, "No Rides Found");
+            }
+        }
+
+        //Method to return invoice summary of a particular user by providing id(UC5)
+        public UserCabInvoiceService ReturnInvoicefromRideRepository(int userId)
+        {
+            return new UserCabInvoiceService(userCabRides[userId], userCabInvoice[userId]);
+        }
+    }
+}
