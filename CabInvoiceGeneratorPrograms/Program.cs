8e0d99c5e130cba16015456cef3064d8bf5a86bb
neeladri22 <neeladri22@gmail.com>
2022-07-22 12:43:44 -0400

    Added program to calculate fare for a journey -UC1

--- CabInvoiceGeneratorPrograms/Program.cs
+++ CabInvoiceGeneratorPrograms/Program.cs
@@ -7,6 +7,10 @@ namespace CabInvoiceGeneratorPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Cab Invoice Generator Program");
+
+            CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator(RideType.NORMAL);
+            Console.WriteLine(cabInvoiceGenerator.CalculateFare(10, 15));
+            Console.ReadLine();
         }
     }
 }
